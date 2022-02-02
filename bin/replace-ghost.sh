11340d44b4a30ae1f1898de0b079b35921877a7d
Federico Lopez L <flopezluksenberg@gmail.com>
2022-02-01 21:30:06 -0300

    Fix replace-ghost.sh

--- bin/replace-ghost.sh
+++ bin/replace-ghost.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Replacing ghost"
-rm -fr node_modules/ghost/core/*
-cp -fr ghost/* node_modules/ghost/core/
+rm -fr node_modules/ghost/core/built
+rm -fr node_modules/ghost/core/server/web/
+cp -fr ghost/built node_modules/ghost/core/built
+cp -fr ghost/server/web node_modules/ghost/core/server/web
