a821d55df46ae59738f9e1fb89c7c07ad4c42d11
Federico Lopez L <flopezluksenberg@gmail.com>
2022-02-01 21:39:26 -0300

    Updated ghost

--- bin/replace-ghost.sh
+++ bin/replace-ghost.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Replacing ghost"
-rm -fr node_modules/ghost/core/built
-rm -fr node_modules/ghost/core/server/web/
-cp -fr ghost/built node_modules/ghost/core/built
-cp -fr ghost/server/web node_modules/ghost/core/server/web
+rm -fr node_modules/ghost/core/*
+cp -fr ghost/* node_modules/ghost/core/
